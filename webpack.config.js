f20cdf99ae209ca2b244d169350f6e0c0e8d6bb5
Thomas Prochazka <thomas.prochazka@gmail.com>
2016-07-12 20:06:35 +0200

    Exclude node_modules from webpack bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
     // Tell webpack, that we want to compile for a node.js environment
     target: 'node',
     entry: './src/floob.js',
+    externals: [nodeExternals()],
     output: {
         path: __dirname,
         filename: './dist/floob.js'
