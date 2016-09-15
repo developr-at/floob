122482886f1abd152db20f0b90d9d7cca3bc0bf8
Thomas Prochazka <thomas.prochazka@gmail.com>
2016-09-15 15:27:48 +0200

    Extend tests for options

--- test/option/option.spec.js
+++ test/option/option.spec.js
@@ -5,14 +5,33 @@ import Option from '../../src/option/option'
 describe('Option Component', () => {
 
     it('should recognize --url or -u option.', () => {
-        var url = 'http://www.google.at';
-        var args = ['--url', url];
-        var options = Option.parse(args);
+        const url = 'http://www.google.at';
+
+        let options = Option.parse(['--url', url]);
         expect(options.url).to.equal(url);
 
-        args = ['-u', url];
-        options = Option.parse(args);
+        options = Option.parse(['-u', url]);
         expect(options.url).to.equal(url);
     });
 
+    it('should recognize --help or -h option.', () => {
+        let options = Option.parse(['--help']);
+        expect(options.help).to.be.true;
+
+        // options = Option.parse([]);
+        // expect(options.help).to.be.false;
+
+        options = Option.parse(['-h']);
+        expect(options.help).to.be.true;
+    });
+
+    it('should recognize --config or -c option.', () => {
+        const configPath = 'dummy';
+
+        let options = Option.parse(['--config', configPath]);
+        expect(options.config).to.equal(configPath);
+
+        options = Option.parse(['-c', configPath]);
+        expect(options.config).to.equal(configPath);
+    });
 });
